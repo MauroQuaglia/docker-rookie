1a1216848df45153eb33986bd39d94e6d805aa88
nautilus@octopus <nautilus@trovaprezzi.it>
2023-08-04 12:30:19 +0200

    Prova

---
+++ tool/check-up.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+echo -e "${RED}-----------------------------------------------------------------------${NOCOLOR}"
+echo -e "${RED}Vediamo le versioni dei vari compnenti di Docker e se sono compatibili:${NOCOLOR}"
+echo -e "${RED}-----------------------------------------------------------------------${NOCOLOR}"
+docker version
+
+echo -e "${RED}----------------------------------------------------------${NOCOLOR}"
+echo -e "${RED}Vediamo le informazioni relative all'ambiente sottostante:${NOCOLOR}"
+echo -e "${RED}----------------------------------------------------------${NOCOLOR}"
+docker system info
+
+echo -e "${RED}---------------------------------------------------------------------------------------------------${NOCOLOR}"
+echo -e "${RED}Ricordati che in /var/lib/docker ci sono immagini e container e molto altro... ma solo se sei root!:${NOCOLOR}"
+echo -e "${RED}Ricordati che in /etc/docker c'è la configurazione del demone!:${NOCOLOR}"
+echo -e "${RED}---------------------------------------------------------------------------------------------------${NOCOLOR}"
