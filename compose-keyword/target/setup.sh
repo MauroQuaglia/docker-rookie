879837d09df7d24e88422e6cdc4aff5687e576f1
nautilus@octopus <nautilus@trovaprezzi.it>
2023-08-28 17:34:18 +0200

    Fix.

---
+++ compose-keyword/target/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+docker-compose down --remove-orphans
+#docker image prune -af
+docker container prune -f
+docker volume prune -af
+docker network prune -f
+docker-compose build #--no-cache
+docker-compose up -d
+docker container exec -it $1 /bin/bash
