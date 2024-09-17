173c046233c6bcedeea9d135fa406af4fafab525
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2024-09-17 16:28:42 +0200

    Refactor

--- images/images.md
+++
@@ -1,5 +0,0 @@
-https://docs.docker.com/reference/dockerfile/
-
-
-# Be Small
-* Dove c'è un Dockerfile ci deve essere un .dockerignore.
--- images/images.sh
+++ images/images.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
 
 # Scarico immagine
-echo "PULL IMAGE"
+echo "---------- PULL IMAGE ---------- "
 docker image pull ubuntu:latest
 
 # Controllo le caratteristiche. Per esempio nel cmd o entrypoint vedo per cosa è designata, più tante altre cose.
-echo "INSPECT IMAGE"
+echo "---------- INSPECT IMAGE ---------- "
 docker image inspect ubuntu:latest
 
 # E' un modo di vedere i comandi di come è stata costruita, quasi come se guardassi il suo Dockerfile.
-echo "HISTORY IMAGE"
+echo "---------- HISTORY IMAGE ---------- "
 docker image history ubuntu:latest
 
 # Per vedere per quali piattaforme è disponibile (oltre alla mia, altrimenti neanche l'avrebbe scaricata)
-echo "MANIFEST IMAGE"
+echo "---------- MANIFEST IMAGE ---------- "
 docker manifest inspect ubuntu:latest
-
-# Creo immagine
-# Quando creo io l'immagine, dare una controllatina va sempre bene:
-  # inspect
-  # history
-# Il manifest non è necessario crearlo.
-# Cercare di tenere l'immagine piccola: un solo RUN, il multi-stage build, ...
-# Usare il no-install-recommends nel apt-get install.
