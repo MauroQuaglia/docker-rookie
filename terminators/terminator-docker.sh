97c019ecaad9b397746c2d8e26b357144344cf09
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2024-09-27 15:07:45 +0200

    Fix

--- terminators/terminator-docker.sh
+++ terminators/terminator-docker.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+cp ~/mauro-quaglia/docker-rookie/ansible/roles/terminator/files/docker_vinc.config ~/.config/terminator/docker_vinc.config
+
 terminator --config ~/.config/terminator/docker_vinc.config --maximise --layout=Docker
--- terminators/terminator-monitoring.sh
+++ terminators/terminator-monitoring.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+cp ~/mauro-quaglia/docker-rookie/ansible/roles/terminator/files/docker_monitor.config ~/.config/terminator/docker_monitor.config
+
 terminator --config ~/.config/terminator/docker_monitor.config --maximise --layout="Docker Monitor"
