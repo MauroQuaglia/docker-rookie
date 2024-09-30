fa9f63f51a46c3173ab0720863e30d96909e8a22
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2024-09-30 15:40:17 +0200

    pg

--- projects/postgresql/compose.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-docker container stop my-pg-container
-docker container rm my-pg-container
-
-docker image build -t my-pg-image .
-docker container run --name my-pg-container --env POSTGRES_PASSWORD=777 --publish 5555:7777 --detach my-pg-image
