bb93e1a25141b6623e0c0168b53c3981f6d144b9
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2024-10-08 15:51:09 +0200

    portainer

--- tools/portainer/portainer.sh
+++ tools/portainer/portainer.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
 
-sudo docker run -d \
+# https://docs.portainer.io/start/install-ce/server/docker/linux
+
+docker container stop portainer
+docker container rm portainer
+docker volume rm portainer_data
+
+docker volume create portainer_data
+docker run \
+    -d \
     -p 9000:9000 \
-    --name=portainer \
+    --name portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data portainer/portainer-ce
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
