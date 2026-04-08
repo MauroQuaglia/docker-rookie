05069fca59a14d987494077520d34ca7faa5063a
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2026-04-08 12:09:16 +0200

    Prove

---
+++ testcontainers/selenium_2_spec.rb
@@ -0,0 +1,27 @@
+require 'testcontainers/selenium'
+require "selenium-webdriver"
+
+describe 'Selenium' do
+  before do
+    @selenium_container = Testcontainers::SeleniumContainer.new(capabilities: :firefox, vnc_no_password: true)
+    @selenium_container.start
+  end
+
+  it 'test' do
+    #Il WebDriver è il ponte tra il codice di test e il browser (Firefox, in questo caso). Si occupa di inviare comandi per navigare, cliccare, prendere screenshot e altro.
+    # #Il driver che sfrutta il container. In questo caso, il container ospita un'istanza di un browser (come Firefox), ma il driver Selenium esegue il codice fuori dal container, controllando e interagendo con il browser all'interno del container. Il container esegue il browser, mentre Selenium WebDriver agisce come una "scatola nera" che invia comandi al browser nel container tramite una connessione URL.
+    driver = Selenium::WebDriver.for(
+      :remote,
+      url:  @selenium_container.selenium_url,
+      options: Selenium::WebDriver::Options.firefox
+    )
+
+    driver.navigate.to("https://www.google.com")
+    expect(driver.title).to eq("Google")
+  end
+
+  after do
+    @selenium_container.stop if @selenium_container
+  end
+
+end
